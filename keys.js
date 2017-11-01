83ace6f14e9748caea5961bbbd5f929b78a0c9a0
octobass <kjlesane@hotmail.com>
2017-11-01 00:06:21 -0500

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.twitterKeys = {
+  consumer_key: 'txP06y4Cy69MhyTzGw7esNPMv',
+  consumer_secret: 'FYvGSOsCQl8iGsa1DxMx0KX9zoUsJ0QIrauoYtyL4bJg3Ipg9S',
+  access_token_key: '887157353363197952-XLw1oohi2RbRBJ3NPbTs8Ty0W225etI',
+  access_token_secret: 'lTAOvvQpSWWMCiSs62xZpfqZ5RKrC6jwS73iWy0h0wZQ0',
+};
+
+
+exports.spotifyKeys = {
+	  id: '33a490ba034942c1b065886e8805a8cd',
+  secret: 'f612c9d1907a478ea574a45f6dcd1de8'
+};
